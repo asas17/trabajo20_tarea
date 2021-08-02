9574dbc8612ed06dd95a7e767846e385dc312d0f
alan almeida <flamigeral2@gmail.com>
2021-08-02 18:05:33 -0500

    Todas las funciones hechas en clases-tarea

---
+++ cmake-build-debug/CMakeFiles/Trabajo18_2.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Trabajo18_2.dir/main.cpp.obj"
+  "Trabajo18_2.exe"
+  "Trabajo18_2.exe.manifest"
+  "Trabajo18_2.pdb"
+  "libTrabajo18_2.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Trabajo18_2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
