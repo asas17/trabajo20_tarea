66c4fa0d824ac35695c41b5404d9a1ca7b23e1a6
alan almeida <flamigeral2@gmail.com>
2021-08-08 11:49:13 -0500

    Actualizado hasta lo que se ha hecho hasta la última clase, junto a la tarea

--- main.cpp
+++ main.cpp
@@ -1,13 +1,12 @@
 #include <iostream>
 #include<stdlib.h>
 #include<time.h>
-
+#include<windows.h>
 using namespace std;
 
 void leer_vector(double vector[], double tamanio) {
     for (int i = 0; i < tamanio; i++) {
-        cout << endl << "Ingrese el elemento #" << (i + 1) << ": ";
-        cin >> vector[i];
+        vector[i] = rand();
     }
 }
 
@@ -79,7 +78,18 @@ void ordenar(double vector[], double tamanio) {
         }
     }
 }
-
+void ordenar_alrevez(double vector[], double tamanio) {
+    for (int i = 0; i < tamanio; i++) {
+        for (int j = i + 1; j < tamanio; j++) {
+            if (vector[i] < vector[j]) {
+                double aux;
+                aux = vector[i];
+                vector[i] = vector[j];
+                vector[j] = aux;
+            }
+        }
+    }
+}
 bool ver(double vector[], double max) {
     int b = true;
     for (int x = 0; x < max; x++) {
@@ -108,11 +118,79 @@ int pos(double vector[], double max) {
     return b;
 }
 
+bool buscar(double array[], int tamanio, int elemento) {
+    for (int i = 0; i < tamanio; i++) {
+        if (array[i] == elemento)
+            return true;
+    }
+    return false;
+}
+
+int buscar_pos(double array[], int tamanio, int elemento) {
+    for (int i = 0; i < tamanio; i++) {
+        if (array[i] == elemento)
+            return i;
+    }
+    return -1;
+}
+
+int cantidad_elemento_vector(double num[], double tamanio, int elemento) {
+    int acum = 0;
+    for (int i = 0; i < tamanio; i++) {
+        if (num[i] == elemento)
+            acum = acum + 1;
+    }
+    return acum;
+}
+
+void intercambio(double &num1, double &num2) {
+    double aux;
+    aux = num1;
+    num1 = num2;
+    num2 = aux;
+
+}
+void ordenar_intercambio(double vector[], double tamanio) {
+    for (int i = 0; i < tamanio; i++) {
+        for (int j = i + 1; j < tamanio; j++) {
+            if (vector[i] > vector[j]) {
+                intercambio(vector[i], vector[j]);
+            }
+        }
+    }
+}
+void nvector(string nombres[], int tamanio) {
+    for (int k = 0; k < tamanio; k++) {
+        cout << endl << nombres[k];
+    }
+}
+void ordenar2(string vector[], int tamanio) {
+    for (int i = 0; i < tamanio; i++) {
+        for (int j = i + 1; j < tamanio; j++) {
+            if (vector[i].compare(vector[j]) < 0) {
+                string aux;
+                aux = vector[i];
+                vector[i] = vector[j];
+                vector[j] = aux;
+            }
+        }
+    }
+}
+double performancecounter_diff(LARGE_INTEGER *a, LARGE_INTEGER *b)
+{
+    LARGE_INTEGER freq;
+    QueryPerformanceFrequency(&freq);
+    return (double)(a->QuadPart - b->QuadPart) / (double)freq.QuadPart;
+}
 int main() {
     srand(time(NULL));
-    const int max = 3;
+    LARGE_INTEGER t_ini, t_fin;
+    double secs;
+    const int max = 6;
     double num[max];
     leer_vector(num, max);
+    num[0] = 10;
+    num[1] = 10;
     imprime_vector(num, max);
     double suma = sumatoria_vector(num, max);
     double promedioo = promedio(num, max);
@@ -142,10 +220,44 @@ int main() {
     } else {
         cout << endl << "La posicion del numero entero encontrado es: " << bb << endl;
     };
+    //Buscar elemento en el vector
+    int y = 10;
+    if (buscar(num, max, y)) {
+        cout << endl << "El elemento " << y << " si existe...";
+    } else {
+        cout << endl << "El elemento " << y << " no existe...";
+    }
+    //
+    int pos = buscar_pos(num, max, y);
+    if (pos == -1)
+        cout << endl << "No existe el elemento " << y << " en el vector...";
+    else
+        cout << endl << "El elemento " << y << " existe en la posicion: " << pos;
+    //diseñe la funcion que devuelva la cantidad de veces que existe un entero en el vector
+    //vector (10, 45, 6, 10, 8, 10)
+    cout << endl << "El elemento " << y << " existe: " << cantidad_elemento_vector(num, max, y) << " veces";
+
     cout << endl << "Ordenar los valores del vector:" << endl;
     cout << endl << "Desordenado";
     imprime_vector(num, max);
-    cout << endl << endl << "Ordenado";
+    cout << endl << endl << "Ordenado ascendente";
+    QueryPerformanceCounter(&t_ini);
     ordenar(num, max);
+    QueryPerformanceCounter(&t_fin);
+    secs = performancecounter_diff(&t_fin, &t_ini);
+    cout<<"\nORDENACION SECUENCIAL: Tiempo EJECUCION en ms: "<<(secs*1000.0);
     imprime_vector(num, max);
+    cout<<endl<<endl<<"Ordenado ascendente por intercambio";
+    ordenar_intercambio(num, max);
+    imprime_vector(num, max);
+    cout << endl << endl << "Ordenado descendente";
+    ordenar_alrevez(num, max);
+    imprime_vector(num, max);
+    const int tam=6;
+    string nombres[tam] = {"Juan", "Antonio","Carlos","Martha","Pedro","Amalia"};
+    cout<<endl<<endl<<"Vector de nombres";
+    nvector(nombres,tam);
+    cout<<endl<<endl<<"Se ordenara el vector string de manera descendente"<<endl;
+    ordenar2(nombres,tam);
+    nvector(nombres,tam);
 }
